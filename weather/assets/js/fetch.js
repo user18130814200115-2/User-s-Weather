2c0f7e17a0a38c301ac00ddf7708d0b00cf1b3a2
user18130814200115-2 <66143190+user18130814200115-2@users.noreply.github.com>
2020-09-16 10:51:30 +0200

    Add files via upload

--- weather/assets/js/fetch.js
+++ weather/assets/js/fetch.js
@@ -3,11 +3,14 @@ var today;
 var hour;
 
 var day = 0;
+var url = "https://wttr.in/?format=j1"
 
 load();
 
+
+
 function LoadRemoteData() {
-    fetch("https://wttr.in/?format=j1")
+    fetch(url)
     .then(function (response) {
         return response.json();
     })
@@ -24,20 +27,24 @@ function updateHours() {
     var desc = jsondata.weather[day].hourly[hour].weatherDesc[0].value;
     //document.getElementById("visual").innerHTML = twemoji.parse(emoji[desc]);
     document.getElementById("visual").innerHTML = "<img class='emoji' alt=\'" + desc + "\' src=assets/emoji/" + emoji[desc] + ">";
-	document.getElementById("hourly").innerHTML = "<br><b>Hourly:</b><br>" + jsondata.weather[day].hourly[hour].weatherDesc[0].value + "<br>" + jsondata.weather[day].hourly[hour].chanceofrain + "% chance of rain<br>" + jsondata.weather[day].hourly[hour].chanceofovercast + "% chance of overcast";
+	document.getElementById("hourly").innerHTML = "<b>Hourly:</b><br>" + jsondata.weather[day].hourly[hour].weatherDesc[0].value + "<br>" + jsondata.weather[day].hourly[hour].chanceofrain + "% chance of rain<br>" + jsondata.weather[day].hourly[hour].chanceofovercast + "% chance of overcast";
 	document.getElementById("software-keys-center").innerHTML = (hour+0)*3 + ":00 - " + (hour+1)*3 + ":00";
 }
 
 function printData() {
-	document.getElementById("daily").innerHTML = "<b>Weather in " + jsondata.nearest_area[0].areaName[0].value + " " + jsondata.nearest_area[0].region[0].value + "</b><br>" + jsondata.weather[day].date + ":<br>" + jsondata.weather[day].mintempC + "-" + jsondata.weather[day].maxtempC + " C";
+	document.getElementById("daily").innerHTML = "<b>Weather in<input id='placeinput' type='text' placeholder='" + jsondata.nearest_area[0].areaName[0].value.replace(/[^\w\s]/gi, '') + " " + jsondata.nearest_area[0].region[0].value + "'></b>" + jsondata.weather[day].date + ":<br>" + jsondata.weather[day].mintempC + "-" + jsondata.weather[day].maxtempC + " C";
 	updateHours();
+	var input = document.getElementById('placeinput');
+		input.focus();
+		input.select();
 }
 
 function reload() {
 	
 	if (navigator.onLine == false) {
-		document.getElementById("error").innerHTML = "Device is not connected to the internet";
+		alert("Device is not connected to the internet");
 	}
+		
 	today = new Date();
   	hour = Math.floor(today.getHours()/3);
 
@@ -80,5 +87,10 @@ document.onkeypress = function (e) {
     }
     else if (e.keyCode == 38) {
     	reload();
+    }
+    else if (e.keyCode == 13) {
+    	url = "https://wttr.in/" + document.getElementById('placeinput').value + "?format=j1";
+    	document.getElementById('placeinput').value = "",
+    	LoadRemoteData();
     }
 };
